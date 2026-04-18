3e0dc2d4d5da810b541b6c138572f1702f87b0f6
AdwaitDeshmukh <66303441+AdwaitDeshmukh@users.noreply.github.com>
2026-04-18 17:32:31 +0530

    merger  resolve

--- chatbot-backend/config/groq.js
+++ chatbot-backend/config/groq.js
@@ -1,10 +1,10 @@
 import Groq from 'groq-sdk';
 import dotenv from 'dotenv';
-//testing
+
 dotenv.config();
 
 const groqClient = new Groq({
-  apiKey: process.env.GROQ_API_KEY,
+  apiKey: process.env.GROQ_API_KEY_2,
 });
 
 export default groqClient;
